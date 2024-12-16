0c4ca48b28fbbe5afae63913f0f8af96b3f2e489
McDonald Ibekwe <mcdonaldibekwe2429@gmail.com>
2024-12-16 21:33:35 +0100

    Use of vector to classify the students

--- main.cpp
+++ main.cpp
@@ -1,36 +1,44 @@
 #include <iostream>
 #include <map>
 #include <format>
+#include <algorithm>
+#include <vector>
 
 class Student {
 public:
-	Student(const std::string& name, int id)
-		: mName{ name }, mId{ id }
+	Student(const std::string& name, int id, double grade)
+		: mName{ name }, mId{ id }, mGrade{ grade }
 	{
-	}
-	bool operator<(const Student& other) const
-	{
-		return mId < other.mId;
 	}
 	void printStudent() const
 	{
-		std::cout << std::format("{}: {}\n{}: {}\n", "Name", mName, "Id", mId);
+		std::cout << std::format("{}: {}\n{}: {}\n{}: {}\n",
+			"Name", mName, "Id", mId, "Grade", mGrade
+		);
+	}
+	double GetGrade() const 
+	{
+		return mGrade;
 	}
 private:
 	std::string mName;
 	int mId;
+	double mGrade;
 };
 
 int main()
 {
-	std::map<Student, double> students{
-		{Student{"Michael Cook", 119}, 4.79},
-		{Student{"Liam Ferguson", 134}, 4.58},
-		{Student{"Timothy Jonathan", 152}, 4.96}
+	std::vector<Student> students{
+		Student{"Michael Cook", 119, 4.79},
+		Student{"Liam Ferguson", 134, 4.58},
+		Student{"Timothy Jonathan", 152, 4.96}
 	};
-	for (const auto& [student, grade] : students) {
+	auto comp = [](const Student& lhs, const Student& rhs) {
+		return lhs.GetGrade() > rhs.GetGrade();
+	};
+	std::sort(students.begin(), students.end(), comp);
+	for(const auto& student: students) {
 		student.printStudent();
-		std::cout << "Grade: " << grade << '\n';
 		std::cout << "==========================================\n";
 	}
 }
